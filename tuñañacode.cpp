7540d60d49b7da84db749d4ff26ff3623fa01896
Matias <mikevinlex777@gmail.com>
2026-04-14 21:20:11 -0500

    Create tuñañacode.cpp

---
+++ tuñañacode.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "Ingrese dos numeros: ";
+    cin >> a >> b;
+    cout << "El resultado de la suma es: " << a + b << endl;
+    return 0;
+}   
